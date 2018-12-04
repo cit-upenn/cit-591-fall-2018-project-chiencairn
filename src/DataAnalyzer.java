9b6bcb5819aa5b4d65da2b9f52afe10c68174d07
Graham Pitcairn <grahampitcairn@huntsman-ve502-0106.apn.wlan.upenn.edu>
2018-12-04 14:45:32 -0500

    Added DataAnalyzer and Tester classes

---
+++ src/DataAnalyzer.java
@@ -0,0 +1,73 @@
+import java.util.ArrayList;
+
+/**
+ * The DataAnalyzer class is used to assess input data in the form of arraylists and produce tangible ratings based on event frequency.
+ * @author grahampitcairn
+ * @version 1.0 12-03-2018
+ */
+public class DataAnalyzer {
+	
+	//Instance Variables
+	double latitude;
+	double longitude;
+	private ArrayList<Crime> crimes;
+	private ArrayList<Litter> litter;
+	final double areaOfPhilly = 141.7;
+	
+	/**
+	 * The DataAnalyzer constructor accepts as arguments: arraylists of data indices and a position. 
+	 * @param c
+	 * @param l
+	 * @param latitude
+	 * @param longitude
+	 */
+	public DataAnalyzer(ArrayList<Crime> c, ArrayList<Litter> l, double latitude, double longitude) {
+		crimes = c;
+		litter = l;
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+	
+	/**
+	 * The getCrimeRating method produces an integer rating between 0-100 based on a comparison to the average crime rate per square mile in the city of Philadelphia
+	 * @return crimeRating
+	 */
+	public int getCrimeRating() {
+		double crimeTally = 0;
+		double crimeRating = 0;
+		for(Crime crime: crimes) {
+			//Comparison of user input location with all data events. [.007]=.5 statute miles in latitude/longitude. 
+			//This creates a 1 square mile box around the user input location
+			if(java.lang.Math.abs(crime.getLatitude()-latitude) < .007 && java.lang.Math.abs(crime.getLongitude() - longitude) < .007) {
+				crimeTally++;
+			}
+		}
+		System.out.println("This position crime tally: "+crimeTally);
+		double avgCrimeRate = crimes.size()/areaOfPhilly;
+		System.out.println("Average crime rate (crimes/m^2): "+avgCrimeRate);
+		crimeRating = 100 - 20*(crimeTally/avgCrimeRate)*(crimeTally/avgCrimeRate);
+		return ((int)crimeRating);
+		
+	}
+	/**
+	 * The getLitterRating method produces an integer rating between 0-100 based on a comparison to the average litter rate per square mile in the city of Philadelphia
+	 * @return litterRating
+	 */
+	public int getLitterRating() {
+		double litterTally = 0;
+		double litterRating = 0;
+		for(Litter litter1: litter) {
+			//Comparison of user input location with all data events. [.007]=.5 statute miles in latitude/longitude. 
+			//This creates a 1 square mile box around the user input location
+			if(java.lang.Math.abs(litter1.getLatitude()-latitude) < .007 && java.lang.Math.abs(litter1.getLongitude() - longitude) < .007) {
+				litterTally++;
+			}
+		}
+		System.out.println("This position litter tally: "+litterTally);
+		double avgLitterRate = litter.size()/areaOfPhilly;
+		System.out.println("Average crime rate (litter/m^2): "+avgLitterRate);
+		litterRating = 100 - 15*(litterTally/avgLitterRate);
+		return ((int)litterRating);
+		
+	}
+}
---
+++ src/Tester.java
@@ -0,0 +1,42 @@
+import java.text.ParseException;
+import java.util.ArrayList;
+
+public class Tester {
+	public static void main(String[] args) {
+		LitterParser lit;
+		CrimeParser crim;
+		try {
+			lit = new LitterParser("litter_index_survey.csv");
+			crim = new CrimeParser("incidents_part1_part2 (1).csv");
+			
+			ArrayList<Litter> litter = lit.getLitter();
+			ArrayList<Crime> crimes = crim.getCrimes();
+			
+			/**
+			 * Print to test first 10 lines
+			 *
+			for(int i = 0; i < 10; i++) {
+				System.out.println("Litter ID: " + litter.get(i).getLitterId()+" Location: "+litter.get(i).getLongitude()+"N, "+litter.get(i).getLatitude()+"W.");
+				System.out.println("Crime ID: "+crimes.get(i).getCrimeId()+" Offense: "+crimes.get(i).getOffense()+" Location: "+crimes.get(i).getLongitude()+"N, "+crimes.get(i).getLatitude()+"W.");
+			}
+			*/
+			
+			
+			/**
+			 * Sample Positions
+			 * Fairmount: 39.963296, -75.172907 crimeRating: 81 litterRating: 87
+			 * UPenn: 39.953164, -75.194880 crimeRating: 78 litterRating: 92
+			 * Kensington: 39.988791, -75.147774 crimeRating: 25 litterRating: 15
+			 * Port Richmond: 40.007622, -75.161062 crimeRating: 43 litterRating: 70
+			 * Rawnhurst: 40.058409, -75.057235 crimeRating: 96 litterRating: 92
+			 */
+			DataAnalyzer da = new DataAnalyzer(crimes, litter, 40.007622, -75.161062);
+			System.out.println(da.getCrimeRating());
+			System.out.println(da.getLitterRating());
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+}
