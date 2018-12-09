e5605cf88f13c519601bd401cc291aa6e2c0104d
Michelle Chien <michellechien@huntsman-ve507-0867.apn.wlan.upenn.edu>
2018-12-08 20:06:11 -0500

    Removed my Google Maps key

--- src/GoogleAPIKey.java
+++ src/GoogleAPIKey.java
@@ -4,7 +4,7 @@
  *
  */
 public class GoogleAPIKey {
-	final static String key = "AIzaSyB3QPLCnP2AMCvlwl8XMr4qJtpN1_-gbYs";
+	final static String key = "KEY_HERE";
 	final static String startURL = "https://maps.googleapis.com/maps/api/geocode/json?address=";
 	final static String endURL = "&key=" + key;
 }
